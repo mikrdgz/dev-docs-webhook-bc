bff3ddd97e01aabd5e14ba528e39f7e1d0ad5f50
mikrdgz <mikrdgz@gmail.com>
2019-03-06 12:35:48 -0600

    added styles and ejs home

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+
+    app.get('/', function (req, res) {
+    res.render('home');
 })
     app.post('/webhooks', function (req, res) {
     res.send('OK')
 })
 app.listen(3000, function () {
-    console.log('Listening for webhooks on port 3000')
 })
