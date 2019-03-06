cc34cf98dbc0390cbd7e3227acd7ba4e1d6f0611
mikrdgz <mikrdgz@gmail.com>
2019-03-06 17:53:09 -0600

    handling response

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+var router = express.Router();
+
 
 
     app.get('/', function (req, res) {
     res.render('home');
 })
-    app.post('/webhooks', function (req, res) {
-    res.send('OK');
-    console.log(req.body);
 
+app.get('/webhooks', function (req, res) {
+res.render('home');
 })
+
+app.post('/webhooks', function (req, res) {
+      res.send('OK');
+      console.log(req.body.data);
+
+  })
+
+
 app.listen(3000, function () {
 })
