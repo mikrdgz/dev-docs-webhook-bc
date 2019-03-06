1b79fdbe0d68212409bc03e5f7c902bd41239738
mikrdgz <mikrdgz@gmail.com>
2019-03-06 13:14:30 -0600

    installing bodyParser and console logging response from BigC

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express()
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
     app.get('/', function (req, res) {
     res.render('home');
 })
     app.post('/webhooks', function (req, res) {
-    res.send('OK')
+    res.send('OK');
+    console.log(req.body);
+
 })
 app.listen(3000, function () {
 })
