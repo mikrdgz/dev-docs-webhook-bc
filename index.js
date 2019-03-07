c9c2cb29cf8a4c157cb1d65f60a1fdaa9e9c6121
mikrdgz <mikrdgz@gmail.com>
2019-03-06 21:32:30 -0600

    ahhhHHHH

--- index.js
+++ index.js
@@ -4,25 +4,62 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 var router = express.Router();
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+let friends = [
+    {
+      name: 'juvi'
+    },
+    {
+      name: 'laura'
+    },
+    {
+    name: 'theo'
+  }
+];
 
-
-    app.get('/', function (req, res) {
-    res.render('home');
-})
 
 app.get('/webhooks', function (req, res) {
-res.render('home');
-})
+   // console.log('get the request');
+   // console.log(req.body.data);
+   // console.log('get the response');
+   // console.log(res);
+   console.log('gotten resposne');
+  res.render('home', {friends});
+});
+
+
+//
+// app.post('/webhooks', function (req, res) {
+//       console.log(req.body)
+//       res.status(200).send(res.body);
+//
+//   })
+
 
 app.post('/webhooks', function (req, res) {
-      res.send('OK');
-      console.log(req.body.data);
+  console.log('post req data');
+  console.log(req.body.data);
+  let newFriend = { name : req.body.data.id };
+  friends.push(newFriend);
+  console.log(friends);
+  res.send(newFriend);
 
+  // const responsebody = res;
+  // const meow = 'theo';
+  // console.log(responsebody);
+  // res.render('home', {
+  //   responseBodyody: responsebody,
+  //   cat: meow
+  // }).status(200);
+});
+
+
+app.listen(3000, function (err) {
+  console.log('we on');
+  if (err) {
+    throw err }
   })
-
-
-app.listen(3000, function () {
-})
